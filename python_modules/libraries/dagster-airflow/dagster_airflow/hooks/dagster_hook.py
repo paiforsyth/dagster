e0cf97eb797c6ff79da6aaefc48e0737d2b7df8a
Joseph Van Drunen <josephvandrunen@gmail.com>
2022-09-30 14:26:47 -0400

    mypy

--- python_modules/libraries/dagster-airflow/dagster_airflow/hooks/dagster_hook.py
+++ python_modules/libraries/dagster-airflow/dagster_airflow/hooks/dagster_hook.py
@@ -39,7 +39,7 @@ class DagsterHook(BaseHook):
     def __init__(
         self,
         dagster_conn_id: Optional[str] = "dagster_default",
-        url: Optional[str] = None,
+        url: str = '',
         user_token: Optional[str] = None,
     ) -> None:
         super().__init__()
@@ -58,7 +58,7 @@ class DagsterHook(BaseHook):
                 "Cannot get user_token: No valid user_token or dagster_conn_id supplied."
             )
 
-        if self.url is None:
+        if self.url == '':
             raise AirflowException(
                 "Cannot get dagster url: No valid url or dagster_conn_id supplied."
             )
